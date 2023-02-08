bbb49b2dffa624d19a869a8b510c6f3af37fdd88
Erlan <erlanamanbekov797@gmail.com>
2023-02-08 16:18:09 +0300

    Finished the calculator

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 export default function Buttons(props) {
-  return (
-    <Button className='btn btn-block'>{props.element}</Button>
+  return (<>
+
+    <Button variant={props.color} name={props.name} style={{width:'25%'}} onClick={props.onClick}>{props.element}</Button>
+
+  </>
   )
+
 }
